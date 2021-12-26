213e1e9bc70b5640a7c28400ae5fe3e6855c8ecc
Vladimir <mikulskiyV@gmail.com>
2021-12-26 19:56:19 +0600

    Если нет кода валюты в списке

--- src/components/CurrencySelect/CurrencySelect.js
+++ src/components/CurrencySelect/CurrencySelect.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { CURRENCIES_NAME } from "../currencyName";
+import "./CurrencySelect.css"
 
 const CurrencySelect = ({choiceCurrency}) => {
 
@@ -22,25 +23,32 @@ const CurrencySelect = ({choiceCurrency}) => {
     };
 
     const gerAllList = () => {
-        return(
-            stateCurrentKeys.map(item => <div 
-                        className="converter-select__item"
-                        onClick={() => choiceCurrency(item)}
-                        key={item + "_COL"}
-                    >
-                    <div className="converter-select__flag" >
-                        <img className="converter-select__img"
-                                src={window.location.origin + "/images/flags/" + item.toLowerCase() + ".png"}
-                                alt={item}
-                        />
+        if(stateCurrentKeys.length){
+            return(
+                stateCurrentKeys.map(item => <div 
+                            className="converter-select__item"
+                            onClick={() => choiceCurrency(item)}
+                            key={item + "_COL"}
+                        >
+                        <div className="converter-select__flag" >
+                            <img className="converter-select__img"
+                                    src={window.location.origin + "/images/flags/" + item.toLowerCase() + ".png"}
+                                    alt={item}
+                            />
+                        </div>
+                        <div className="converter-select__info">
+                            <div className="converter-select__code">{item}</div>
+                            <div className="converter-select__name">{CURRENCIES_NAME[item]}</div>
+                        </div>
                     </div>
-                    <div className="converter-select__info">
-                        <div className="converter-select__code">{item}</div>
-                        <div className="converter-select__name">{CURRENCIES_NAME[item]}</div>
-                    </div>
-                </div>
+                )
             )
-        )
+        }else{
+            return(
+                <div className="converter-select__no-code">Код валюты не найден</div>
+            )
+        }
+
     };
 
     return(
